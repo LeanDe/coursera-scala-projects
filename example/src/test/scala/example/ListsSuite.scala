f28ebaadee83d835100d81645a05f169d57634df
Leandro Deveikis <ldeveikis@despegar.com>
2014-04-29 14:31:59 -0300

    implementing assigment 1

--- example/src/test/scala/example/ListsSuite.scala
+++ example/src/test/scala/example/ListsSuite.scala
@@ -19,7 +19,7 @@ import org.scalatest.junit.JUnitRunner
  * inside eclipse using the built-in JUnit test runner.
  *
  * You have two options for running this test suite:
- * 
+ *
  * - Start the sbt console and run the "test" command
  * - Right-click this file in eclipse and chose "Run As" - "JUnit Test"
  */
@@ -39,7 +39,6 @@ class ListsSuite extends FunSuite {
    */
   test("one plus one is two")(assert(1 + 1 == 2))
 
-
   /**
    * In Scala, it is allowed to pass an argument to a method using the block
    * syntax, i.e. `{ argument }` instead of parentheses `(argument)`.
@@ -47,9 +46,8 @@ class ListsSuite extends FunSuite {
    * This allows tests to be written in a more readable manner:
    */
   test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+    assert(1 + 1 == 2) // This assertion fails! Go ahead and fix it.
   }
-
 
   /**
    * One problem with the previous (failing) test is that ScalaTest will
@@ -72,9 +70,8 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 1 === 2) // Fix me, please!
   }
-
 
   /**
    * In order to test the exceptional behavior of a methods, ScalaTest offers
@@ -94,14 +91,12 @@ class ListsSuite extends FunSuite {
     else x
   }
 
-
   /**
    * Now we finally write some tests for the list functions that have to be
    * implemented for this assignment. We fist import all members of the
    * `List` object.
-   */ 
+   */
   import Lists._
-  
 
   /**
    * We only provide two very basic tests for you. Write more tests to make
@@ -115,10 +110,14 @@ class ListsSuite extends FunSuite {
    * every tested aspect of a method.
    */
   test("sum of a few numbers") {
-    assert(sum(List(1,2,0)) === 3)
+    assert(sum(List(1, 2, 0)) === 3)
   }
-  
+
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+  }
+
+  def sum(x: List) {
+
   }
 }
